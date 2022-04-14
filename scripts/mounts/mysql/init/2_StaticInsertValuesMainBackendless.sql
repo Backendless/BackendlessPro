98774f6ddace463357db913192f137c2d31094f8
Sergii Kukurudziak <sergey@backendless.com>
2022-04-14 09:20:46 +0300

    BKNDLSS-28018 "Root/Web" option should be by default for Web App access

--- scripts/mounts/mysql/init/2_StaticInsertValuesMainBackendless.sql
+++ scripts/mounts/mysql/init/2_StaticInsertValuesMainBackendless.sql
@@ -1,6 +1,6 @@
 USE `main_backendless`;
 
-INSERT INTO `Version` (`main`, `application`) values (57, 102);
+INSERT INTO `Version` (`main`, `application`) values (58, 102);
 
 INSERT INTO `DeveloperStatus` (`id`, `name`) VALUES ('1', 'ACTIVE');
 INSERT INTO `DeveloperStatus` (`id`, `name`) VALUES ('2', 'SUSPENDED');
