5088ba2fd2f5b3e7f0dd95f7e79c15e2009b6039
Sergii Kukurudziak <sergey@backendless.com>
2022-05-31 10:14:12 +0300

    fixed db version for dev

--- scripts/mounts/mysql/init/2_StaticInsertValuesMainBackendless.sql
+++ scripts/mounts/mysql/init/2_StaticInsertValuesMainBackendless.sql
@@ -1,6 +1,6 @@
 USE `main_backendless`;
 
-INSERT INTO `Version` (`main`, `application`) values (60, 104);
+INSERT INTO `Version` (`main`, `application`) values (60, 105);
 
 INSERT INTO `DeveloperStatus` (`id`, `name`) VALUES ('1', 'ACTIVE');
 INSERT INTO `DeveloperStatus` (`id`, `name`) VALUES ('2', 'SUSPENDED');
